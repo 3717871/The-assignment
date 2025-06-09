8f6afad0452ad62d73ade9cf1222fa9895ea046c
Ruben Kruijver <715623@student.inholland.nl>
2025-06-09 14:44:05 +0200

    Modified the controller layer to my MovieService class

--- moviewatchlist/src/main/java/nl/inholland/appliedmathematics/oop3/moviewatchlist/controller/MovieController.java
+++ moviewatchlist/src/main/java/nl/inholland/appliedmathematics/oop3/moviewatchlist/controller/MovieController.java
@@ -1,5 +1,6 @@
 package nl.inholland.appliedmathematics.oop3.moviewatchlist.controller;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
@@ -14,42 +15,138 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-import nl.inholland.appliedmathematics.oop3.moviewatchlist.service.IMovieService;
+import nl.inholland.appliedmathematics.oop3.moviewatchlist.model.Movie;
+import nl.inholland.appliedmathematics.oop3.moviewatchlist.model.MovieDTO;
+import nl.inholland.appliedmathematics.oop3.moviewatchlist.service.MovieService;
 
 @RestController
 @RequestMapping("/api/movie")
 @RequiredArgsConstructor
 public class MovieController {
 
-    private final IMovieService movieService;
-
-    @GetMapping
-    public ResponseEntity<List<Person>> getAllPeople() {
-        return ResponseEntity.ok(personService.getAllPeople());
-    }
+    private final MovieService movieService;
 
     @PostMapping
-    public ResponseEntity<Void> createPerson(@RequestBody PersonDTO personDTO) {
-        personService.createPerson(personDTO);
+    public ResponseEntity<Void> createPerson(@RequestBody MovieDTO movieDTO) {
+        movieService.createMovie(movieDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+        
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Person> updatePerson(@PathVariable UUID id, @RequestBody PersonDTO personDTO) {
-        Person updatedPerson = personService.updatePerson(id, personDTO);
+    @GetMapping
+    public ResponseEntity<List<Movie>> findAllMovies() {
+        return ResponseEntity.ok(movieService.getAllMovies());
+    }
+
+
+    @GetMapping("/{title}")
+    public ResponseEntity<List<Movie>> findMovieByTitle(@PathVariable String title) {
+        return ResponseEntity.ok(movieService.getMovieByTitle(title));
+    }
+
+
+    @GetMapping("/{releaseYear}")
+    public ResponseEntity<List<Movie>> findMovieByReleaseYear(@PathVariable int releaseYear) {
+        return ResponseEntity.ok(movieService.getMovieByReleaseYear(releaseYear));
+    }
+
+
+    @GetMapping("/{director}")
+    public ResponseEntity<List<Movie>> findMovieByDirector(@PathVariable String director) {
+        return ResponseEntity.ok(movieService.getMovieByDirector(director));
+    }
+
+
+    @GetMapping("/{genre}")
+    public ResponseEntity<List<Movie>> findMovieByGenre(@PathVariable String genre) {
+        return ResponseEntity.ok(movieService.getMovieByGenre(genre));
+    }
+
+
+    @GetMapping("/{similarMovies}")
+    public ResponseEntity<List<Movie>> findMovieBySimilarMovies(@PathVariable String similarMovies) {
+        return ResponseEntity.ok(movieService.getMovieBySimilarMovies(similarMovies));
+    }
+
+
+    @GetMapping("/{filePaths}")
+    public ResponseEntity<List<Movie>> findMovieByFilePaths(@PathVariable String filePaths) {
+        return ResponseEntity.ok(movieService.getMovieByFilePaths(filePaths));
+    }
+
+
+    @GetMapping("/{watched}")
+    public ResponseEntity<List<Movie>> findMovieByFileWatched(@PathVariable boolean watched) {
+        return ResponseEntity.ok(movieService.getMovieByWatched(watched));
+    }
+
+
+    @GetMapping("/{rating}")
+    public ResponseEntity<List<Movie>> findMovieByRating(@PathVariable int rating) {
+        return ResponseEntity.ok(movieService.getMovieByRating(rating));
+    }
+
+
+    @PutMapping("/{id}/title")
+    public ResponseEntity<Movie> updateMovieTitleById(@PathVariable UUID id, @RequestBody String title) {
+        Movie updatedPerson = movieService.updateMovieTitleById(title, id);
         return ResponseEntity.ok(updatedPerson);
     }
 
+
+    @PutMapping("/{id}/releaseYear")
+    public ResponseEntity<Movie> updateMovieReleaseYearById(@PathVariable UUID id, @RequestBody int releaseYear) {
+        Movie updatedPerson = movieService.updateMovieReleaseYearById(releaseYear, id);
+        return ResponseEntity.ok(updatedPerson);
+    }
+
+
+    @PutMapping("/{id}/director")
+    public ResponseEntity<Movie> updateMovieDirectorById(@PathVariable UUID id, @RequestBody String director) {
+        Movie updatedPerson = movieService.updateMovieDirectorById(director, id);
+        return ResponseEntity.ok(updatedPerson);
+    }
+
+
+    @PutMapping("/{id}/genre")
+    public ResponseEntity<Movie> updateMovieGenreById(@PathVariable UUID id, @RequestBody String genre) {
+        Movie updatedPerson = movieService.updateMovieGenreById(genre, id);
+        return ResponseEntity.ok(updatedPerson);
+    }
+
+
+    @PutMapping("/{id}/similarMovies")
+    public ResponseEntity<Movie> updateMovieSimilarMoviesById(@PathVariable UUID id, @RequestBody String similarMovies) {
+        Movie updatedPerson = movieService.updateMovieSimilarMoviesById(similarMovies, id);
+        return ResponseEntity.ok(updatedPerson);
+    }
+
+
+    @PutMapping("/{id}/filePaths")
+    public ResponseEntity<Movie> updateMovieFilePathsById(@PathVariable UUID id, @RequestBody String filePaths) {
+        Movie updatedPerson = movieService.updateMovieFilePathsById(filePaths, id);
+        return ResponseEntity.ok(updatedPerson);
+    }
+
+
+    @PutMapping("/{id}/watched")
+    public ResponseEntity<Movie> updateMovieWatchedById(@PathVariable UUID id, @RequestBody boolean watched) {
+        Movie updatedPerson = movieService.updateMovieWatchedById(watched, id);
+        return ResponseEntity.ok(updatedPerson);
+    }
+
+
+    @PutMapping("/{id}/rating")
+    public ResponseEntity<Movie> updateMovieRatingById(@PathVariable UUID id, @RequestBody int rating) {
+        Movie updatedPerson = movieService.updateMovieRatingById(rating, id);
+        return ResponseEntity.ok(updatedPerson);
+    }
+
+
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Void> deletePerson(@PathVariable UUID id) {
-        personService.deletePerson(id);
+    public ResponseEntity<Void> deleteMovie(@PathVariable UUID id) {
+        movieService.deleteMovieById(id);
         return ResponseEntity.noContent().build();
     }
-
-    @GetMapping("/{name}")
-    public ResponseEntity<List<Person>> findPersonByName(@PathVariable String name) {
-        return ResponseEntity.ok(personService.findPersonByName(name));
-    }
-
 
 }
