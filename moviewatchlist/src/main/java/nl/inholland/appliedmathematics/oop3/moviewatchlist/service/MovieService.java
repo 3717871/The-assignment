1b86ecb49f6184ce88fe00e2214286f7e3e2b37d
Ruben Kruijver <715623@student.inholland.nl>
2025-06-10 13:08:06 +0200

    Added Streams to my Movie Service class

--- moviewatchlist/src/main/java/nl/inholland/appliedmathematics/oop3/moviewatchlist/service/MovieService.java
+++ moviewatchlist/src/main/java/nl/inholland/appliedmathematics/oop3/moviewatchlist/service/MovieService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -73,53 +74,116 @@ public class MovieService implements IMovieService{
             movieRepository.save(movie);
             System.out.println("Finished the interaction with the DB: " + System.currentTimeMillis());
         });
-        
     }
+
 
     @Override
     public List<Movie> getAllMovies() {
         return movieRepository.findAll();
     }
 
+
     @Override
     public List<Movie> getMovieByTitle(String title) {
         return movieRepository.findByTitle(title);
     }
 
+
     @Override
     public List<Movie> getMovieByReleaseYear(int releaseYear) {
         return movieRepository.findByReleaseYear(releaseYear);
     }
+
     
     @Override
     public List<Movie> getMovieByDirector(String director) {
         return movieRepository.findByDirector(director);
     }
 
+
     @Override
     public List<Movie> getMovieByGenre(String genre) {
         return movieRepository.findByGenre(genre);
     }
+
 
     @Override
     public List<Movie> getMovieBySimilarMovies(String similarMovies) {
         return movieRepository.findBySimilarMovies(similarMovies);
     }
 
+
     @Override
     public List<Movie> getMovieByFilePaths(String filePaths) {
         return movieRepository.findByFilePaths(filePaths);
     }
+
 
     @Override
     public List<Movie> getMovieByWatched(boolean watched) {
         return movieRepository.findByWatched(watched);
     }
 
+
     @Override
     public List<Movie> getMovieByRating(int rating) {
         return movieRepository.findByRating(rating);
     }
+
+
+    public List<String> getMovieNamesByRatingBound(int bound, boolean higher){
+
+        List<Movie> movies = movieRepository.findAll();
+
+        List<String> filtered = new ArrayList<>();
+
+        if(higher){
+            filtered = movies.stream()
+            .filter(m -> m.getRating() >= bound)
+            .map(Movie::getTitle)
+            .sorted()
+            .collect(Collectors.toList());
+        } else {
+            filtered = movies.stream()
+            .filter(m -> m.getRating() < bound)
+            .map(Movie::getTitle)
+            .sorted()
+            .collect(Collectors.toList());
+        }
+
+        return filtered;
+    }
+
+
+    public boolean isBetweenYearBounds(int lower, int upper){
+
+        List<Movie> movies = movieRepository.findAll();
+
+        boolean isBetween = movies.stream()
+        .anyMatch(m -> m.getReleaseYear() >= lower && m.getReleaseYear() <= upper);
+
+        return isBetween;
+    }
+
+
+    public float getDirectorRating(String director){
+
+        List<Movie> movies = movieRepository.findAll();
+
+        List<Integer> ratings = movies.stream()
+        .filter(m -> m.getDirector().equals(director))
+        .map(Movie::getRating)
+        .collect(Collectors.toList());
+
+        int ratingSum = 0;
+
+        for(int i = 0; i < ratings.size(); i++){
+            ratingSum += ratings.get(i);
+        }
+
+        return ratingSum / ratings.size();
+    }
+
 
     @Override
     public Movie updateMovieTitleById(String title, UUID id) {
@@ -128,12 +192,14 @@ public class MovieService implements IMovieService{
         return movieRepository.save(movie);
     }
 
+
     @Override
     public Movie updateMovieReleaseYearById(int releaseYear, UUID id) {
         Movie movie = movieRepository.findById(id).orElseThrow(() -> new MovieNotFoundException("No movie found with id: " + id));
         movie.setReleaseYear(releaseYear);
         return movieRepository.save(movie);
     }
+
 
     @Override
     public Movie updateMovieDirectorById(String director, UUID id) {
@@ -142,12 +208,14 @@ public class MovieService implements IMovieService{
         return movieRepository.save(movie);
     }
 
+
     @Override
     public Movie updateMovieGenreById(String genre, UUID id) {
         Movie movie = movieRepository.findById(id).orElseThrow(() -> new MovieNotFoundException("No movie found with id: " + id));
         movie.setGenre(genre);
         return movieRepository.save(movie);
     }
+
 
     @Override
     public Movie updateMovieSimilarMoviesById(String similarMovies, UUID id) {
@@ -156,12 +224,14 @@ public class MovieService implements IMovieService{
         return movieRepository.save(movie);
     }
 
+
     @Override
     public Movie updateMovieFilePathsById(String filePaths, UUID id) {
         Movie movie = movieRepository.findById(id).orElseThrow(() -> new MovieNotFoundException("No movie found with id: " + id));
         movie.setFilePaths(filePaths);
         return movieRepository.save(movie);
     }
+
 
     @Override
     public Movie updateMovieWatchedById(boolean watched, UUID id) {
@@ -170,12 +240,14 @@ public class MovieService implements IMovieService{
         return movieRepository.save(movie);
     }
 
+
     @Override
     public Movie updateMovieRatingById(int rating, UUID id) {
         Movie movie = movieRepository.findById(id).orElseThrow(() -> new MovieNotFoundException("No movie found with id: " + id));
         movie.setRating(rating);
         return movieRepository.save(movie);
     }
+
 
     @Override
     public void deleteMovieById(UUID id) {
